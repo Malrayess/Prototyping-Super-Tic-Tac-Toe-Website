26029dc0653709e55940187aa0fcae16e3f0057e
malrayess <m.alrayess@share.epsb.ca>
2019-10-30 11:49:29 -0600

    Update

--- Prototyping Super-Tic-Tac-Toe Javascript.js
+++ Prototyping Super-Tic-Tac-Toe Javascript.js
@@ -6,6 +6,8 @@ let fields = "";
 let board = "";
 let BequalD = 3;
 let FequalD = 3;
+let l = 300;
+let Vlines, Hlines;
 var x, y;
 var count = 0;
 var xWin = 0;
@@ -13,7 +15,7 @@ var oWin = 0;
 let theme = "Dark";
 var main = false;
 var BcellD = l/BequalD;
-var FcellD = (l/Vlines)/Math.pow(BequalD, 2);
+var FcellD = Math.pow(BequalD, 2);
 document.body.style.backgroundColor = "black";
 
 //CANVAS 1 SCOREBOARD
@@ -36,34 +38,53 @@ var cheight = 100;
   ctx.fillText("O: ", 100, 50);// text, x, y
 
 //CANVAS 2 PLAYING BOARD
-var canvas = document.getElementById("Canvas2");
-var ctx = canvas.getContext("2d");
+var canvas2 = document.getElementById("Canvas2");
+var ctx2 = canvas2.getContext("2d");
 var length = l;
-let l = 300;
 
   ctx2.fillStyle = "black"; // fills canvas in black
   ctx2.fillRect(0, 0, l, l); // coordinates
   ctx2.strokeStyle = '#FFFFFF'; // fills border in white
   ctx2.strokeRect(0, 0, l, l); // coordinates
   ctx2.lineWidth = 4; // line width
-  ctx2.stroke();
 
+  FDrawlines();
   BDrawlines();
 
   function BDrawlines() {
     for (let Vlines = 1; Vlines < BequalD; Vlines ++) {
-      ctx.strokeStyle = 'white';
-      ctx.moveTo(l*Vlines/BequalD, 0);
-      ctx.lineTo(l*Vlines/BequalD, l);
-      ctx.lineWidth = 7;
-      ctx.stroke();
+      ctx2.beginPath();
+      ctx2.strokeStyle = 'blue';
+      ctx2.moveTo(l*Vlines/BequalD, 0);
+      ctx2.lineTo(l*Vlines/BequalD, l);
+      ctx2.lineWidth = 5;
+      ctx2.stroke();
     }
 
     for (let Hlines = 1; Hlines < BequalD; Hlines ++) {
-      ctx.strokeStyle = 'white';
-      ctx.moveTo(0, l*Hlines/BequalD);
-      ctx.lineTo(l, l*Hlines/BequalD);
-      ctx.lineWidth = 7;
-      ctx.stroke();
+      ctx2.beginPath();
+      ctx2.strokeStyle = 'blue';
+      ctx2.moveTo(0, l*Hlines/BequalD);
+      ctx2.lineTo(l, l*Hlines/BequalD);
+      ctx2.lineWidth = 5;
+      ctx2.stroke();
+    }
+  }
+
+  function FDrawlines() {
+    for (let Vlines = 1; Vlines < FcellD; Vlines++) {
+      ctx2.strokeStyle = 'white';
+      ctx2.moveTo(l*Vlines/FcellD, 0);
+      ctx2.lineTo(l*Vlines/FcellD, l);
+      ctx2.lineWidth = 3;
+      ctx2.stroke();
+    }
+
+    for (let Hlines = 1; Hlines < FcellD; Hlines++) {
+      ctx2.strokeStyle = 'white';
+      ctx2.moveTo(0, l*Hlines/FcellD);
+      ctx2.lineTo(l, l*Hlines/FcellD);
+      ctx2.lineWidth = 3;
+      ctx2.stroke();
     }
   }
